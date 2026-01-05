b80587bc9a450955de09e2001995b4637fa89fc5
Antonín <av@flwn.dev>
2026-01-05 20:22:32 +0100

    fix: moved client config screen registration to client event

--- src/main/java/dev/flwn/placeablemaxwell/platform/neoforge/NeoforgeClientEventSubscriber.java
+++ src/main/java/dev/flwn/placeablemaxwell/platform/neoforge/NeoforgeClientEventSubscriber.java
@@ -12,11 +12,16 @@ import dev.flwn.placeablemaxwell.platform.neoforge.item.HolidayRecord;
 /*import net.minecraft.resources.ResourceLocation;
  *///?} else {
 import net.minecraft.resources.Identifier;
-		//?}
+//?}
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.IExtensionPoint;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 //? if >1.21.3 {
 import net.neoforged.neoforge.client.event.RegisterConditionalItemModelPropertyEvent;
 import net.neoforged.neoforge.client.event.RegisterSelectItemModelPropertyEvent;
@@ -24,6 +29,10 @@ import net.neoforged.neoforge.client.event.RegisterSelectItemModelPropertyEvent;
 //? if <=1.21.3 {
 /*import net.minecraft.client.renderer.item.ItemProperties;
  *///?}
+//? if >1.20.6
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+//? if <=1.20.6
+//import dev.flwn.placeablemaxwell.common.ConfigScreen;
 
 @EventBusSubscriber(
 		modid = MaxwellMod.MOD_ID,
@@ -50,6 +59,16 @@ public class NeoforgeClientEventSubscriber {
 			ItemProperties.register(ModItems.POOMBA_BLOCK.get(), xmasId, (stack, level, entity, seed) -> CheckHoliday.isXmas() ? 1.0f : 0.0f);
 		});
 		*///?}
+
+		ModContainer modContainer = ModList.get()
+				.getModContainerById(MaxwellMod.MOD_ID)
+				.orElseThrow();
+
+		// Config screen setup
+		//? if >1.20.6
+		modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+		//? if <=1.20.6
+		//modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigScreen::new);
 	}
 
 	//? if >1.21.3 {
--- src/main/java/dev/flwn/placeablemaxwell/platform/neoforge/NeoforgeEntrypoint.java
+++ src/main/java/dev/flwn/placeablemaxwell/platform/neoforge/NeoforgeEntrypoint.java
@@ -13,11 +13,6 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
-//? if >1.20.6
-import net.neoforged.neoforge.client.gui.ConfigurationScreen;
-//? if <=1.20.6
-//import dev.flwn.placeablemaxwell.common.ConfigScreen;
 
 @Mod(MaxwellMod.MOD_ID)
 public class NeoforgeEntrypoint {
@@ -28,11 +23,6 @@ public class NeoforgeEntrypoint {
 		ModBlocks.register(modEventBus);
 
 		modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-
-		//? if >1.20.6
-		modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
-		//? if <=1.20.6
-		//modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigScreen::new);
 
 		MaxwellMod.onInitialize();
 	}
